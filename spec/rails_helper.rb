c3b1a33dff0fa78f71547e7ecc088b3d128295df
Boris Kuznetsov <me@achempion.com>
2023-02-20 18:33:11 +0100

    Headless browser configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,8 +31,8 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 
-Capybara.javascript_driver = :selenium_chrome_headless
-Capybara.default_driver = :selenium_chrome_headless
+Capybara.javascript_driver = :selenium_headless
+Capybara.default_driver = :selenium_headless
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
